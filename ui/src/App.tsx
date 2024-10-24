bdc7cb0725eda5be74983a7689ed5582f206d19d
Jingshi Liu <jingshiliu07@gmail.com>
2024-10-24 10:58:20 -0400

    refactor: update ui repo name

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { getBackendUrl } from './util/api-helper';
 
 function App() {
-  const [count, setCount] = useState(0);
-  
+  const [text, setText] = useState<string>();
+
+  useEffect(() => {
+    axios.get(getBackendUrl()).then((rep) => setText(rep.data));
+  }, []);
+
   return (
     <>
       <h1 className="text-xl font-bold underline">Hello World </h1>
+      <p>Backend Text: {text}</p>
     </>
   );
 }
--- ui/src/util/api-helper.ts
+++ ui/src/util/api-helper.ts
@@ -3,3 +3,7 @@ const BACKEND_PORT = import.meta.env.BACKEND_PORT ?? 3031
 export const getBackendPort = () => {
     return BACKEND_PORT
 }
+
+export const getBackendUrl = () => {
+    return `http://localhost:${BACKEND_PORT}`
+}
