fd086b51868feb894308308b57448788977bbe12
Nevin <nevinzheng0921@gmail.com>
2024-10-24 11:29:48 -0400

    Co-authored-by: Jingshi Liu <jingshiliu07@gmail.com>

---
+++ ui/src/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <>
+       <div><h1>404 Not Found</h1></div>
+       <Link to="/">back to Home</Link>
+    </>
+  );
+}
+
+export default NotFound;
---
+++ ui/src/pages/register/RegisterPage.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function RegisterPage() {
+  const [userName,setUserName] = useState('');
+  const [password,setPassword] = useState('');
+  return (
+    <>
+     <h1 className="text-xl font-bold underline">Register Page</h1>
+    <div className='flex justify-center content-center'>
+      
+
+    <h3>Password must contain the following:</h3>
+        <ol id="message" className='list-decimal'>
+            <li id="letter" className="m-5">A <b>lowercase</b> letter</li>
+            <li id="capital" className="m-5">A <b>capital (uppercase)</b> letter</li>
+            <li id="number" className="m-5">A <b>number</b></li>
+            <li id="length" className="m-5">Minimum <b>8 characters</b></li>
+        </ol>
+    </div>
+    </>
+  );
+}
+
+export default RegisterPage;
