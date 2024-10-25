c0adbdcb2b66bfcd1cd4cec89f50536f60123749
Nevin <nevinzheng0921@gmail.com>
2024-10-25 14:07:37 -0400

    found color not added, now added

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -3,18 +3,47 @@ export default {
     darkMode: ["class"],
     content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
+  	screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: {
+        DEFAULT: '#fdfffcff',  // Base primary color
+      },
+      second: {
+        DEFAULT: '#235789ff',
+      },
+      third: {
+        DEFAULT: '#c1292eff',
+      },
+      fourth: {
+        DEFAULT: '#f1d302ff',
+      },
+      fivth: {
+        DEFAULT: '#020100ff',  // Green for success messages
+      }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
   	}
   },
   plugins: [require("tailwindcss-animate")],
 }
 
+ 
