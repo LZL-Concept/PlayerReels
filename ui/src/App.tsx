1d0978d7e08db51f636412e6b3b65b61a86618ed
Nevin <nevinzheng0921@gmail.com>
2024-10-26 11:29:26 -0400

    added login link to main page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { getBackendUrl } from './util/api-helper';
+import { Link } from 'react-router-dom';
 
 function App() {
   const [text, setText] = useState<string>();
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <h1 className="text-xl font-bold underline">Hello World </h1>
+      <Link to='/Login'>Login</Link>
       <p>Backend Text: {text}</p>
     </>
   );
